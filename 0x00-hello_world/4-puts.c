a317c0ab148d1b6cb9f7359753e70f81f21ccbca
stackish <101517864+stackish@users.noreply.github.com>
2022-09-11 20:58:25 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-char Mystr[] = "\"Programming is like building a multilingual puzzle"
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
 puts(Mystr);
 return 0;
 }
